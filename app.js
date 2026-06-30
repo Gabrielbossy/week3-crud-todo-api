d897ddb26c7a399301c8597f7d1fece360fe1e0a
Gabrielbossy <gabrielotich@gmail.com>
2026-06-30 21:45:24 +0300

    Configure app for Render deployment

--- app.js
+++ app.js
@@ -87,5 +87,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
